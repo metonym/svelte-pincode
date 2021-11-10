b10aecbe0c632a65a2ed7819058f8d545fb4f45f
metonym <ericyl.us@gmail.com>
2021-11-10 10:46:41 -0800

    breaking(build): drop support for es/umd builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,21 @@
-import resolve from "@rollup/plugin-node-resolve";
-import svelte from "rollup-plugin-svelte";
 import svelteReadme from "svelte-readme";
-import pkg from "./package.json";
 
-export default () => {
-  if (!process.env.BUNDLE) return svelteReadme({
-    style: `
-      [data-pincode], [data-pincode] ~ div {
-        margin-bottom: 16px;
-      }
+export default svelteReadme({
+  style: `
+    [data-pincode], [data-pincode] ~ div {
+      margin-bottom: 16px;
+    }
 
-      .complete {
-        font-weight: 700;
-      }
+    .complete {
+      font-weight: 700;
+    }
 
-      .success {
-        color: #24a148;
-      }
+    .success {
+      color: #24a148;
+    }
 
-      .error {
-        color: #da1e28;
-      }
-   `
-  });
-
-  return ["es", "umd"].map((format) => {
-    const UMD = format === "umd";
-
-    return {
-      input: pkg.svelte,
-      output: {
-        format,
-        file: UMD ? pkg.main : pkg.module,
-        name: UMD ? pkg.name : undefined,
-        exports: "named",
-      },
-      plugins: [svelte({ emitCss: false }), resolve()],
-    };
-  });
-};
+    .error {
+      color: #da1e28;
+    }
+ `,
+});
